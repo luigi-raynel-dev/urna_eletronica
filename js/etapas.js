8d0aad034b80e77ac76937423af96f1ae4e5b188
Luigi Raynel <devluigiraynel@gmail.com>
2020-12-23 13:19:21 -0300

    first commit of true

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Bruno',
+                partido: 'BRN',
+                fotos:[
+                    {url:'img/Bruno-38111.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Carla',
+                partido: 'CRL',
+                fotos:[
+                    {url:'img/Carla-77222.jpeg', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome:'Daniela',
+                partido: 'DEF',
+                vice:"Felipe",
+                fotos:[
+                    {url:'img/Daniela-99.jpeg', legenda: 'Prefeito', small: false},
+                    {url:'img/Felipe-99_2.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome:'João',
+                partido: 'JEM',
+                vice:"Maria",
+                fotos:[
+                    {url:'img/Joao-84.jpeg', legenda: 'Prefeito', small: false},
+                    {url:'img/Maria-84_2.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
---
+++ js/script.js
@@ -0,0 +1,102 @@
+// controle da interface
+let num = document.querySelector('.info-voto');
+let cargo = document.querySelector('.info-2 span');
+let aviso = document.querySelector('.scr-2');
+let fotos = document.querySelector('.foto');
+let votoInfo = document.querySelector('.info-1 span');
+let descricao = document.querySelector('.info-dados');
+
+
+// controle de ambiente
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let votos = [];
+
+
+function comecaEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numHTML = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i = 0; i < etapa.numeros; i++){
+        if (i === 0) {
+            numHTML += '<div class="numero pisca"></div>'
+        }
+        else{
+            numHTML += '<div class="numero"></div>'
+        }
+    }
+
+    votoInfo.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    fotos.innerHTML = '';
+    num.innerHTML = numHTML;
+}
+
+function atualizaTela() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+
+        votoInfo.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br>Partido: ${candidato.partido}`;
+        aviso.style.display = 'block';
+
+        let fotosHTML = '';
+        for(let i = 0; i < candidato.fotos.length; i++) {
+            if(i === 0) {
+                fotosHTML += `<div class="image"><img src="${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
+        }
+        fotos.innerHTML = fotosHTML;
+    }
+    else{
+        votoInfo.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso-grande pisca">VOTO NULO</div>';
+    }
+    
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        }
+        else{
+            atualizaTela();
+        }
+    }
+}
+
+function branco() {
+    alert("Clicou em Branco")
+}
+
+function corrige() {
+    alert("Clicou em corrige")
+}
+
+function confirma() {
+    alert("Clicou em confirma")
+}
+
+comecaEtapa();
